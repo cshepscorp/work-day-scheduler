cc298d71bed2ea4c0b4a1af2c6371504856df615
cshepscorp <sheppard.christy@gmail.com>
2021-09-26 20:00:43 -0400

    initial framework set up

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var currentDay = document.querySelector("#current-day");
+var hour09 = document.querySelector("#hour-09");
+
+currentDay.textContent = moment().format('dddd, MMMM Do');
+
+// function todaysDate() {
+//     currentDay.textContent = moment().format('dddd, MMMM Do');
+// }
+
+// todaysDate();
